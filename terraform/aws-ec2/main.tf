e23573c5e87b174fd27fe52fa89292cdace2f243
Yeshwanth Madineni <yeshwanth.global@gmail.com>
2026-06-08 18:43:09 -0500

    Add Terraform EC2 key pair and Docker bootstrap

--- terraform/aws-ec2/main.tf
+++ terraform/aws-ec2/main.tf
@@ -50,6 +50,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
+  key_name               = "sre-ops-key"
   user_data              = file("${path.module}/scripts/install-docker.sh")
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
